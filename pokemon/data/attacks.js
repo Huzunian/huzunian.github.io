ec22923e91647564286255874831ea1aae291c80
Huzunian <99221369+Huzunian@users.noreply.github.com>
2022-11-21 09:36:09 -0600

    Add files via upload

---
+++ pokemon/data/attacks.js
@@ -0,0 +1,14 @@
+const attacks = {
+    Tackle: {
+        name: 'Tackle',
+        damage: 25,
+        type: 'Normal',
+        color: 'gray'
+    },
+    Fireball: {
+        name: 'Fireball',
+        damage: 50,
+        type: 'Fire',
+        color: 'red'
+    }
+}
