424bba4f8e8b326a2ee5c0b89b88c25d991f30df
Huzunian <99221369+Huzunian@users.noreply.github.com>
2022-11-21 09:23:16 -0600

    Update huzunian.js

--- ccmods/huzunian.js
+++ ccmods/huzunian.js
@@ -1,1 +1,2 @@
 Game.LoadMod("https://raw.githubusercontent.com/Huzunian/huzunian.github.io/main/ccmods/cookiemonster.js");
+Game.LoadMod("https://raw.githubusercontent.com/Huzunian/huzunian.github.io/main/ccmods/frozencookies.js");
