2ba9a760bb13063f15f7ee97419b2223f5204726
Huzunian <99221369+Huzunian@users.noreply.github.com>
2022-11-21 16:43:20 -0600

    Delete ccmods directory

--- ccmods/huzunian.js
+++
@@ -1,2 +0,0 @@
-Game.LoadMod("https://raw.githubusercontent.com/Huzunian/huzunian.github.io/main/ccmods/cookiemonster.js");
-Game.LoadMod("https://raw.githubusercontent.com/Huzunian/huzunian.github.io/main/ccmods/frozencookies.js");
