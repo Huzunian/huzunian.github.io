fe4462b8327dd2f577ea4f5fc200719d474ff653
Frogzunian <99221369+Huzunian@users.noreply.github.com>
2023-01-01 11:55:40 -0600

    Update source.js

--- blookcheat/source.js
+++ blookcheat/source.js
@@ -1,3 +1,6 @@
 function bcjs#getDailyRewards() {
   document.write('<script type="text/javascript" src="https://raw.githubusercontent.com/Huzunian/Blooket-Cheats/main/obfuscated/global/getDailyRewards.js"></script>');
 }
+function bcjs#getDailyRewards() {
+  document.write('<script type="text/javascript" src="https://raw.githubusercontent.com/Huzunian/Blooket-Cheats/main/obfuscated/global/getDailyRewards.js"></script>');
+}
