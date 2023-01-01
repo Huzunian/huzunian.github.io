fb5883735cb8f6bf57fb2b83ea1657d81b119c37
Frogzunian <99221369+Huzunian@users.noreply.github.com>
2023-01-01 13:06:41 -0600

    Update source.js

--- blookcheat/source.js
+++ blookcheat/source.js
@@ -1,3 +1,24 @@
+(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Avo/avo.min.js';document.body.appendChild(a);}())
+function bcjsHelp() {
+  console.log('---')
+  console.log('Functions');
+  console.log('---')
+  console.log('bcjsHelp()')
+  console.log('Logs this list again')
+  console.log('---')
+  console.log('bcjsGui()')
+  console.log('Loads Computer GUI')
+  console.log('---')
+  console.log('bcjsMobileGui()')
+  console.log('Loads Mobile GUI')
+  console.log('---')
+  console.log('Functions')
+  console.log('Functions')
+  console.log('---')
+  console.log('Functions')
+  console.log('Functions')
+  console.log('---')
+}
 function bcjsGui() {
   document.write('<script type="text/javascript" src="https://raw.githubusercontent.com/Huzunian/Blooket-Cheats/main/obfuscated/Gui.js"></script>');
 }
